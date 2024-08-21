0e3f3c308c45e49e46d843b0c250b3e5e46c8f9b
Skaiste Kankalyte <skaiste.ka@gmail.com>
2024-08-21 18:39:30 +0300

    NeonDB Implementation & API routes

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,6 +1,5 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { SignedIn, useUser } from "@clerk/clerk-expo";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
